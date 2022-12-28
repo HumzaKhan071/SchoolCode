b01530bfb4680ca1c6f45fa614fcfd1be9379015
Peter Oti <71887594+PetxCode@users.noreply.github.com>
2022-12-28 13:23:56 +0100

    little changes

--- src/Components/DashBoard/AdminDash/Screen/Academics.tsx
+++ src/Components/DashBoard/AdminDash/Screen/Academics.tsx
@@ -101,7 +101,7 @@ function Academics() {
                         alignItems: "center",
                       }}
                     >
-                      <div>Position </div> : Teacher data
+                      <div>Position </div> : Teacher
                     </div>
 
                     <Cal>
