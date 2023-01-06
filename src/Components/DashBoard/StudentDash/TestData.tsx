9c7b63c2154ce0a6480ae82b611d298fc056f531
gideonekekeke <gideonekeke64@gmail.com>
2023-01-06 01:02:26 +0100

    create test

--- src/Components/DashBoard/StudentDash/TestData.tsx
+++ src/Components/DashBoard/StudentDash/TestData.tsx
@@ -1,296 +1,290 @@
-interface demain  {
-    question:string,
-    answer:string,
-    options:{}[]
-
-
+interface demain {
+	question: string;
+	answer: string;
+	options: {}[];
 }
 
 interface TestMap {
-    subjectTest:string,
-    time:"2hrs",
-    testDetails: any,
-    gradeScore:number,
-    id:number
-
-
+	subjectTest: string;
+	time: "2hrs";
+	testDetails: any;
+	gradeScore: number;
+	id: number;
 }
 
-export const TestData: TestMap[]  = [
+export const TestData: TestMap[] = [
+	{
+		id: 1,
+		subjectTest: "English Test1",
+		time: "2hrs",
+		testDetails: [
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+		],
+		gradeScore: 5,
+	},
 
-    {
-        "id":1,
-        "subjectTest": "English Test1",
-         "time": "2hrs", 
-         "testDetails": [
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             }
-         ], 
-         "gradeScore": 5
-    },
+	{
+		id: 2,
+		subjectTest: "Maths Test1",
+		time: "2hrs",
+		testDetails: [
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+		],
+		gradeScore: 5,
+	},
 
-    {
-        "id":2,
-        "subjectTest": "Maths Test1",
-         "time": "2hrs", 
-         "testDetails": [
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             }
-         ], 
-         "gradeScore": 5
-    },
-
-    {
-        "id":3,
-        "subjectTest": "Biology Test1",
-         "time": "2hrs", 
-         "testDetails": [
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             }
-         ], 
-         "gradeScore": 5
-    },
-    {
-        "id":4,
-        "subjectTest": "Pyhics Test1",
-         "time": "2hrs", 
-         "testDetails": [
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             },
-             {
-                 "question": "Who is the President of Nigeria",
-                 "answer": "Buhari",
-                 "options": [
-                     {"a": "Obasenjo"},
-                     {"b": "Buhari"},
-                     {"c": "Goodluck"},
-                     {"d": "Peter"}
-                 ]
-             }
-         ], 
-         "gradeScore": 5
-    }
-    
-]
+	{
+		id: 3,
+		subjectTest: "Biology Test1",
+		time: "2hrs",
+		testDetails: [
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+		],
+		gradeScore: 5,
+	},
+	{
+		id: 4,
+		subjectTest: "Pyhics Test1",
+		time: "2hrs",
+		testDetails: [
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+			{
+				question: "Who is the President of Nigeria",
+				answer: "Buhari",
+				options: [
+					{ a: "Obasenjo" },
+					{ b: "Buhari" },
+					{ c: "Goodluck" },
+					{ d: "Peter" },
+				],
+			},
+		],
+		gradeScore: 5,
+	},
+];
